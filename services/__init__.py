c5f3df32c991be0a26c3ff83163414299e3567ed
Denis Indenbom <59681620+denisindenbom@users.noreply.github.com>
2024-09-24 18:51:36 +0300

    Some changes after review

--- services/__init__.py
+++ services/__init__.py
@@ -1,1 +1,1 @@
-from .scripts import get_shape, get_minimum_and_maximum, read_from_file, get_top_artists, Song
+from .scripts import get_shape, get_minimum_and_maximum, get_top_artists, Song
--- services/scripts.py
+++ services/scripts.py
@@ -1,19 +1,19 @@
 # Music recommendation application
 import csv
 
-from cfg import TableEnum
+from cfg import TableIndexes
 
 
 class Song:
     def __init__(self, row: list[str]) -> None:
-        self.id = int(row[TableEnum.ID.value])
-        self.name = row[TableEnum.Track.value]
-        self.album = row[TableEnum.Album.value]
-        self.artist = row[TableEnum.Artist.value]
+        self.id = row[TableIndexes.ID.value]
+        self.name = row[TableIndexes.TRACK.value]
+        self.album = row[TableIndexes.ALBUM.value]
+        self.artist = row[TableIndexes.ARTIST.value]
 
 
 class DataBase:
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.db = {}
 
         with open(filename, encoding='utf-8-sig') as file:
@@ -21,41 +21,46 @@ class DataBase:
             reader = csv.reader(file)
 
             for row in reader:
-                self.add_song(Song(row))
+                self.add_song(self.__parse_row(row))
+
+    @staticmethod
+    def __parse_row(row: list[str]) -> Song:
+        row[TableIndexes.ID.value] = int(row[TableIndexes.ID.value])
+
+        return Song(row)
 
     def add_song(self, song: Song) -> None:
+        if song.id in self.db:
+            raise 'A song with this id already exists!'
+
         self.db[song.id] = song
 
-    def get_song(self, song_id: int) -> Song:
+    def get_song(self, song_id: int) -> Song | None:
         return self.db.get(song_id)
-
-
-if __name__ == '__main__':
-    db = DataBase(filename='../data/Spotify_Youtube.csv')
-
-    print(db.get_song(1).name)
 
 
 def get_top_artists(data: list[list[str]], n: int) -> list[tuple[str, float]]:
     artists = {}
     likes = {}
+
     for row in data:
-        artists[row[TableEnum.Artist.value]] = artists.get(row[TableEnum.Artist.value], 0) + 1
-        likes[row[TableEnum.Artist.value]] = (
-            likes.get(row[TableEnum.Artist.value], 0) + float(row[TableEnum.Likes.value])
-            if row[TableEnum.Likes.value] != ""
+        artists[row[TableIndexes.ARTIST.value]] = artists.get(row[TableIndexes.ARTIST.value], 0) + 1
+        likes[row[TableIndexes.ARTIST.value]] = (
+            likes.get(row[TableIndexes.ARTIST.value], 0) + float(row[TableIndexes.LIKES.value])
+            if row[TableIndexes.LIKES.value] != ""
             else 0
         )
 
     for artist in artists:
         artists[artist] = likes[artist] / artists[artist]
 
-    return sorted(artists.items(), key=lambda x: x[1], reverse=True)[:n]
+    return sorted(artists.items(), key=lambda x:x[1], reverse=True)[:n]
 
 
 def get_minimum_and_maximum(data: list[list[str]], index: int) -> tuple[float, float]:
     if index < 0 or index >= len(data[0]):
         raise IndexError("Index out of range")
+
     if not data[0][index].replace(".", "", 1).isdigit():
         raise ValueError("Column must be a digit")
 
@@ -65,3 +70,9 @@ def get_minimum_and_maximum(data: list[list[str]], index: int) -> tuple[float, f
 
 def get_shape(data: list[list[str]]) -> tuple[int, int]:
     return len(data), len(data[0])
+
+
+if __name__ == '__main__':
+    db = DataBase(filename='../data/Spotify_Youtube.csv')
+
+    print(db.get_song(1).name)
